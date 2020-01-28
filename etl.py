6bdad8cef7ab3302bd62af2ca28dcbb8b2d492dd
Daniel Tavares <daniel.mts@gmail.com>
2020-01-28 21:39:44 +0000

    refactor all execution (table creation and data loading) to etl, and add typing hints

--- etl.py
+++ etl.py
@@ -1,29 +1,56 @@
-import configparser
-import psycopg2
+import argparse
+from psycopg2.extensions import cursor, connection
+
+from settings import get_config
+from db import get_connection
+from create_tables import drop_tables, create_tables
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-def load_staging_tables(cur, conn):
+def load_staging_tables(cur: cursor, conn: connection) -> None:
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def insert_tables(cur, conn):
+def insert_tables(cur: cursor, conn: connection) -> None:
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    parser = argparse.ArgumentParser(
+        description="Sparkify ETL"
+    )
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    option_group = parser.add_mutually_exclusive_group()
+
+    option_group.add_argument(
+        "--create-tables",
+        action="store_true",
+        help="Create Tables",
+    )
+
+    option_group.add_argument(
+        "--load-data",
+        action="store_true",
+        help="Load Data",
+    )
+
+    args = parser.parse_args()
+
+    config = get_config()
+
+    conn = get_connection(config["CLUSTER"])
     cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+
+    if args.create_tables:
+        drop_tables(cur, conn)
+        create_tables(cur, conn)
+    elif args.load_data:
+        load_staging_tables(cur, conn)
+        insert_tables(cur, conn)
 
     conn.close()
 
